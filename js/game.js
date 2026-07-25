8ff2740eb9fc0b70e1017a8338bcbdcd895ca1d9
Frederik Stoltenhoff <frederik.stoltenhoff@outlook.de>
2026-07-25 22:15:26 +0200

    feat: render first graphic on the canvas

--- js/game.js
+++ js/game.js
@@ -1,4 +1,14 @@
+let canvas;
+let ctx;
+let character = new Image();
+
 function init() {
     // Initialization code for the game
-    
+    canvas = document.getElementById('canvas');
+    ctx = canvas.getContext('2d');
+    character.src = '../img/2_character_pepe/2_walk/W-21.png';
+
+    setTimeout( function() {
+    ctx.drawImage(character, 100, 100, 50, 150);
+}, 1000);
 }
