d79d9610b7e50f3c9d53ed0f7d1dea594b3941bc
Frederik Stoltenhoff <frederik.stoltenhoff@outlook.de>
2026-08-03 17:25:33 +0200

    feat: refactor movable object structure and add character classes

--- js/game.js
+++ js/game.js
@@ -1,14 +1,12 @@
 let canvas;
 let ctx;
-let character = new Image();
+let character = new MovableObject();
 
 function init() {
     // Initialization code for the game
     canvas = document.getElementById('canvas');
     ctx = canvas.getContext('2d');
-    character.src = '../img/2_character_pepe/2_walk/W-21.png';
 
-    setTimeout( function() {
-    ctx.drawImage(character, 100, 100, 50, 150);
-}, 1000);
+
+console.log('My Character is:', character);
 }
