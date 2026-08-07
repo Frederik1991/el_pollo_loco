94e81d7e74724145568d2f55f9f43e35b56251f9
Frederik Stoltenhoff <frederik.stoltenhoff@outlook.de>
2026-08-07 22:52:54 +0200

    feat: move fixed dimensions for Cloud class to class properties

--- models/clouds.class.js
+++ models/clouds.class.js
@@ -1,13 +1,14 @@
 class Cloud extends MovableObject {
 
+    withd = 400; // Set a fixed width for clouds
+    height = 300; // Set a fixed height for clouds
+
     constructor() {
         super();
         this.loadImage('img/5_background/layers/4_clouds/1.png');
 
         this.x = 50 + Math.random() * 700; // Random x position between 50 and 750
         this.y = 20 + Math.random() * 60; // position clouds near top
-        this.width = 400; // Set a fixed width for clouds
-        this.height = 300; // Set a fixed height for clouds
     }
 
     
