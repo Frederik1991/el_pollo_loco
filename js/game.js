3ec3400f09fe381857f1302b0213950cc2f1ba74
Frederik Stoltenhoff <frederik.stoltenhoff@outlook.de>
2026-08-07 21:57:33 +0200

    refactor: reorganize initialization and structure of World and MovableObject classes

--- js/game.js
+++ js/game.js
@@ -1,14 +1,12 @@
 let canvas;
-let ctx;
+let world;
 
 
-let world = new World ();
 
 function init() {
-    // Initialization code for the game
     canvas = document.getElementById('canvas');
-    ctx = canvas.getContext('2d');
+    world = new World(canvas);
 
 
-console.log('My Character is:', world.character);
+    console.log('My Character is:', world.character);
 }
