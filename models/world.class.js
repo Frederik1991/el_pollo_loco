2576d1646038df93ce66ea18c03bd763e7d91948
Frederik Stoltenhoff <frederik.stoltenhoff@outlook.de>
2026-08-13 22:46:28 +0200

    feat: update BackgroundObject constructor to calculate y position and adjust instantiation in World class

--- models/world.class.js
+++ models/world.class.js
@@ -14,7 +14,7 @@ class World {
     ];
 
     backgroundObjects = [
-        new BackgroundObject('img/5_background/layers/1_first_layer/1.png', 0, 50),
+        new BackgroundObject('img/5_background/layers/1_first_layer/1.png', 0),
         
     ];
 
