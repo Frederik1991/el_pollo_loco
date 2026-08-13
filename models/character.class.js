a0c6f66ca2be28dac165e8b7ac05a9737022bec0
Frederik Stoltenhoff <frederik.stoltenhoff@outlook.de>
2026-08-13 23:15:13 +0200

    feat: add y, width, and height properties to Character and Chicken classes

--- models/character.class.js
+++ models/character.class.js
@@ -1,5 +1,7 @@
 class Character extends MovableObject {
-    
+    y = 130;
+    width = 140;
+    height = 300;
 
     constructor() {
         super().loadImage('img/2_character_pepe/2_walk/W-21.png');
