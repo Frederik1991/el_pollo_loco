65d79ce775e7d9710cc2d5d0c35fc77ff93616ae
Frederik Stoltenhoff <frederik.stoltenhoff@outlook.de>
2026-08-14 15:52:54 +0200

    feat: implement loadImages method in MovableObject class and update Character class to use it

--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -5,9 +5,19 @@ class MovableObject {
     width = 100;
     height = 150;
 
+    imageCache = {};
+
     loadImage(path) {
         this.img = new Image();
         this.img.src = path;
+    }
+
+    loadImages(arr) {
+        arr.forEach((path) => {
+            let img = new Image();  
+            img.src = path;
+            this.imageCache[path] = img;
+        });
     }
 
     moveRight() {
