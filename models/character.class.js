850d8bf21bb22eff884ce0cf6ff5371d670d098c
Frederik Stoltenhoff <frederik.stoltenhoff@outlook.de>
2026-08-14 16:03:51 +0200

    refactor: reorganize image loading in Character class and streamline animate method

--- models/character.class.js
+++ models/character.class.js
@@ -2,17 +2,33 @@ class Character extends MovableObject {
     y = 130;
     width = 140;
     height = 300;
+    IMAGES_WALKING = [
+        'img/2_character_pepe/2_walk/W-21.png',
+        'img/2_character_pepe/2_walk/W-22.png',
+        'img/2_character_pepe/2_walk/W-23.png',
+        'img/2_character_pepe/2_walk/W-24.png',
+        'img/2_character_pepe/2_walk/W-25.png',
+        'img/2_character_pepe/2_walk/W-26.png'
+    ];
+
+    currentImageIndex = 0;
 
     constructor() {
         super().loadImage('img/2_character_pepe/2_walk/W-21.png');
-        this.loadImages = ([
-            'img/2_character_pepe/2_walk/W-21.png',
-            'img/2_character_pepe/2_walk/W-22.png',
-            'img/2_character_pepe/2_walk/W-23.png',
-            'img/2_character_pepe/2_walk/W-24.png',
-            'img/2_character_pepe/2_walk/W-25.png',
-            'img/2_character_pepe/2_walk/W-26.png'
-        ]);
+        this.loadImages(this.IMAGES_WALKING);
+        this.animate();
+    }
+
+    animate() {
+        setInterval(() => {
+            this.currentImageIndex = (this.currentImageIndex + 1) % this.IMAGES_WALKING.length;
+            this.img = this.imageCache[this.IMAGES_WALKING[this.currentImageIndex]];
+        }, 100);
+    }
+
+    jump() {
+
+    
     }
 
     jump() {
