f24513fe6a89e9de941f626b7839b32dd6d6985d
Frederik Stoltenhoff <frederik.stoltenhoff@outlook.de>
2026-08-21 10:22:06 +0200

    fix: format backgroundObjects array for improved readability

--- models/world.class.js
+++ models/world.class.js
@@ -14,7 +14,7 @@ class World {
     ];
 
     backgroundObjects = [
-   new BackgroundObject('img/5_background/layers/air.png', -719),
+        new BackgroundObject('img/5_background/layers/air.png', -719),
         new BackgroundObject('img/5_background/layers/3_third_layer/2.png', -719),
         new BackgroundObject('img/5_background/layers/2_second_layer/2.png', -719),
         new BackgroundObject('img/5_background/layers/1_first_layer/2.png', -719),
@@ -28,15 +28,15 @@ class World {
         new BackgroundObject('img/5_background/layers/2_second_layer/2.png', 719),
         new BackgroundObject('img/5_background/layers/1_first_layer/2.png', 719),
 
-        new BackgroundObject('img/5_background/layers/air.png', 719*2),
-        new BackgroundObject('img/5_background/layers/3_third_layer/1.png', 719*2),
-        new BackgroundObject('img/5_background/layers/2_second_layer/1.png', 719*2),
-        new BackgroundObject('img/5_background/layers/1_first_layer/1.png', 719*2),
-        new BackgroundObject('img/5_background/layers/air.png', 719*3),
-        new BackgroundObject('img/5_background/layers/3_third_layer/2.png', 719*3),
-        new BackgroundObject('img/5_background/layers/2_second_layer/2.png', 719*3),
-        new BackgroundObject('img/5_background/layers/1_first_layer/2.png', 719*3),
-        
+        new BackgroundObject('img/5_background/layers/air.png', 719 * 2),
+        new BackgroundObject('img/5_background/layers/3_third_layer/1.png', 719 * 2),
+        new BackgroundObject('img/5_background/layers/2_second_layer/1.png', 719 * 2),
+        new BackgroundObject('img/5_background/layers/1_first_layer/1.png', 719 * 2),
+        new BackgroundObject('img/5_background/layers/air.png', 719 * 3),
+        new BackgroundObject('img/5_background/layers/3_third_layer/2.png', 719 * 3),
+        new BackgroundObject('img/5_background/layers/2_second_layer/2.png', 719 * 3),
+        new BackgroundObject('img/5_background/layers/1_first_layer/2.png', 719 * 3),
+
     ];
 
     ctx;
