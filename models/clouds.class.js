b55e6119d865e41b5a78e87ff23cebd42f0fb79f
Frederik Stoltenhoff <frederik.stoltenhoff@outlook.de>
2026-08-24 11:27:00 +0200

    feat: update cloud's random x position range for better placement

--- models/clouds.class.js
+++ models/clouds.class.js
@@ -7,7 +7,7 @@ class Cloud extends MovableObject {
         super();
         this.loadImage('img/5_background/layers/4_clouds/1.png');
 
-        this.x = 50 + Math.random() * 700; // Random x position between 50 and 750
+        this.x = 50 + Math.random() * 2500; // Random x position between 50 and 2500
         this.y = 20 + Math.random() * 60; // position clouds near top
 
         this.animate();
