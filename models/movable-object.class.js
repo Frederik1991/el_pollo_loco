8e0b6d5c181d212d0d8038893e9f641e1e5a023d
Frederik Stoltenhoff <frederik.stoltenhoff@outlook.de>
2026-08-24 12:19:00 +0200

    feat: implement draw and drawFrame methods in MovableObject and update rendering in World

--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -40,14 +40,10 @@ class MovableObject {
 
     moveRight() {
         this.x += this.speed;
-        
-
     }
 
     moveLeft() {
         this.x -= this.speed;
-        
-        
     }
 
     playAnimation(images) {
@@ -60,6 +56,18 @@ class MovableObject {
     jump() {
         this.speedY = 30;
         console.log(this.speedY);
+    }
+
+    draw(ctx) {
+        ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
+    }
+
+    drawFrame(ctx) {
+        ctx.beginPath();
+        ctx.linewidth = '10';
+        ctx.strokeStyle = 'blue';
+        ctx.rect(this.x, this.y, this.width, this.height);
+        ctx.stroke();
     }
 
 }
