627fce1fa6d9978cccad2af152fdb92ecee6ef73
Frederik Stoltenhoff <frederik.stoltenhoff@outlook.de>
2026-08-26 11:51:22 +0200

    feat: add dead and hurt states for character with corresponding animations

--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -10,6 +10,7 @@ class MovableObject {
     speedY = 0;
     acceleration = 2.5;
     energy = 100;
+    lastHit = 0;
 
     imageCache = {};
 
@@ -83,6 +84,21 @@ class MovableObject {
 
     hit() {
         this.energy -= 5;
+        if (this.energy < 0) {
+            this.energy = 0;
+        } else {
+            this.lastHit = new Date().getTime();
+        }
+    }
+
+    isHurt() {
+        let timepassed = new Date().getTime() - this.lastHit;
+        timepassed = timepassed / 1000;
+        return timepassed < 1;
+    }
+
+    isDead() {
+        return this.energy == 0;
     }
 
 }
