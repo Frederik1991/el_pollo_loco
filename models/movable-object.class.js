d4fe7bc29ac0bf5e97d744dd4f575e3b7bc6d2c2
Frederik Stoltenhoff <frederik.stoltenhoff@outlook.de>
2026-08-28 11:04:11 +0200

    feat: enhance isAboveGround method to account for ThrowableObject instances

--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -18,7 +18,11 @@ class MovableObject extends DrawableObject {
     }
 
     isAboveGround() {
+        if(this instanceof ThrowableObject) {
+            return true;
+        } else {
         return this.y < 130;
+        }
     }
 
     moveRight() {
--- models/throwable-object.class.js
+++ models/throwable-object.class.js
@@ -1,12 +1,23 @@
 class ThrowableObject extends MovableObject {
     x = 100;
     y = 150;
-    width = 100;
-    height = 100;
+    width = 60;
+    height = 80;
 
     constructor() {
         super();
         this.loadImage('img/6_salsa_bottle/salsa_bottle.png')
+        this.throw(100, 100);
+    }
+
+    throw(x, y) {
+        this.x = x;
+        this.y = y;
+        this.speedY = 20;
         this.applyGravity();
+        setInterval(() => {
+            this.x += 10;
+        }, 25);
+
     }
 }
