54c26b735605ff589ac1abf72d1b24700b041824
Frederik Stoltenhoff <frederik.stoltenhoff@outlook.de>
2026-08-30 14:06:09 +0200

    feat: update Coin class to fix dimensions and improve animation logic

--- models/coin.class.js
+++ models/coin.class.js
@@ -1,25 +1,28 @@
 class Coin extends MovableObject {
 
-    width = 100; // Set a fixed width for clouds
-    height = 100; // Set a fixed height for clouds
+    width = 100; // Set a fixed width for coins
+    height = 100; // Set a fixed height for coins
+
+    IMAGES = [
+        'img/8_coin/coin_1.png',
+        'img/8_coin/coin_2.png'
+    ];
 
     constructor() {
-    super();
-    this.loadImage('img/8_coin/coin_1.png');
+        super().loadImage('img/8_coin/coin_1.png');
+        this.loadImages(this.IMAGES);
 
-    this.x = 50 + Math.random() * 2300; // Random x position between 50 and 2500
-    this.y = 360; // position Bottles near bottom
+        this.x = 200 + Math.random() * 2300; // Random x position between 50 and 2500
+        this.y = 50 + Math.random() * 100;; // position coins
 
-    this.animate();
-
-}
+        this.animate();
+    }
 
     animate() {
 
         setInterval(() => {
-            this.moveLeft();
-            this.x -= this.speed; // Move clouds to the left
-        }, 1000 / 60); // 60 frames per second
-      
+            this.playAnimation(this.IMAGES);
+        }, 300); // 60 frames per second
+
     }
 }
