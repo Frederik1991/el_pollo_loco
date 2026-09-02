ffe3bcfb71265399e6e6dc0505238dc77fa939a5
Frederik Stoltenhoff <frederik.stoltenhoff@outlook.de>
2026-09-02 21:12:23 +0200

    feat: add rotation images and animation for ThrowableObject class

--- models/throwable-object.class.js
+++ models/throwable-object.class.js
@@ -2,12 +2,25 @@ class ThrowableObject extends MovableObject {
     width = 60;
     height = 80;
 
+    IMAGES_ROTATE = [
+        'img/6_salsa_bottle/bottle_rotation/1_bottle_rotation.png',
+        'img/6_salsa_bottle/bottle_rotation/2_bottle_rotation.png',
+        'img/6_salsa_bottle/bottle_rotation/3_bottle_rotation.png',
+        'img/6_salsa_bottle/bottle_rotation/4_bottle_rotation.png'
+    ]
+
+    IMAGES_SPLASH = [
+        
+    ]
+
     constructor(x, y) {
         super();
         this.loadImage('img/6_salsa_bottle/salsa_bottle.png')
+        this.loadImages(this.IMAGES_ROTATE);
         this.y = y;
         this.x = x;
         this.throw();
+        this.animate();
     }
 
     throw() {
@@ -16,6 +29,11 @@ class ThrowableObject extends MovableObject {
         setInterval(() => {
             this.x += 10;
         }, 25);
+    }
 
+    animate() {
+        setInterval(() => {
+            this.playAnimation(this.IMAGES_ROTATE);
+        }, 100);
     }
 }
