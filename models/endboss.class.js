1cc333964ff3fe092efec0c1253dc899797cb0d9
Frederik Stoltenhoff <deine.email@beispiel.de>
2026-09-05 12:35:05 +0200

    feat: add walking, attack, hurt, and dead images arrays to Endboss class

--- models/endboss.class.js
+++ models/endboss.class.js
@@ -4,6 +4,13 @@ class Endboss extends MovableObject {
     height = 500;
     y = -10;
 
+     IMAGES_WALKING = [
+        'img/4_enemie_boss_chicken/1_walk/G1.png',
+        'img/4_enemie_boss_chicken/1_walk/G2.png',
+        'img/4_enemie_boss_chicken/1_walk/G3.png',
+        'img/4_enemie_boss_chicken/1_walk/G4.png'
+    ]
+
     IMAGES_ALERT = [
         'img/4_enemie_boss_chicken/2_alert/G5.png',
         'img/4_enemie_boss_chicken/2_alert/G6.png',
@@ -13,6 +20,18 @@ class Endboss extends MovableObject {
         'img/4_enemie_boss_chicken/2_alert/G10.png',
         'img/4_enemie_boss_chicken/2_alert/G11.png',
         'img/4_enemie_boss_chicken/2_alert/G12.png',
+    ]
+
+    IMAGES_ATTACK = [
+
+    ]
+
+    IMAGES_HURT = [
+
+    ]
+
+    IMAGES_DEAD = [
+
     ]
 
     constructor() {
