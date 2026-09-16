fe737602a39ad38b464dfec9422131ea88820c6d
Frederik Stoltenhoff <frederik.stoltenhoff@outlook.de>
2026-09-16 14:01:25 +0200

    refactor: improve constructor documentation for BackgroundObject class

--- models/backgound-object.class.js
+++ models/backgound-object.class.js
@@ -1,9 +1,17 @@
+/**
+ * Represents a static background layer object positioned along the x-axis.
+ */
 class BackgroundObject extends MovableObject {
 
     width = 720;
     height = 480;
 
-    constructor(imagePath, x, ) {
+    /**
+     * Creates a new background object and loads its image.
+     * @param {string} imagePath - Path to the background image.
+     * @param {number} x - Horizontal position of the background object.
+     */
+    constructor(imagePath, x) {
         super().loadImage(imagePath);
         this.x = x;
         this.y = 480 - this.height;
