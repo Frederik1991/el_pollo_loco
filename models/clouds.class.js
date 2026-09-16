8d6322989933efeab6b5d344be11286865944b54
Frederik Stoltenhoff <frederik.stoltenhoff@outlook.de>
2026-09-16 16:53:49 +0200

    style: add documentation comments for Cloud class and its methods

--- models/clouds.class.js
+++ models/clouds.class.js
@@ -1,24 +1,29 @@
+/**
+ * Represents a background cloud that slowly drifts across the sky.
+ */
 class Cloud extends MovableObject {
 
     width = 600; // Set a fixed width for clouds
     height = 250; // Set a fixed height for clouds
 
+    /**
+     * Creates a new cloud at a random position and starts its movement.
+     */
     constructor() {
         super();
         this.loadImage('img/5_background/layers/4_clouds/1.png');
-
         this.x = 50 + Math.random() * 2500; // Random x position between 50 and 2500
         this.y = 20 + Math.random() * 60; // position clouds near top
-
         this.animate();
     }
 
+    /**
+     * Continuously moves the cloud to the left.
+     */
     animate() {
-
         setInterval(() => {
             this.moveLeft();
             this.x -= this.speed; // Move clouds to the left
         }, 1000 / 60); // 60 frames per second
-      
     }
 }
