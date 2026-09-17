63bdb2eac08cef5cb222dd39b6e1cdf4bfbc18ec
Frederik Stoltenhoff <frederik.stoltenhoff@outlook.de>
2026-09-17 19:40:24 +0200

    style: add documentation comments for Level class and its constructor

--- models/level.class.js
+++ models/level.class.js
@@ -1,3 +1,7 @@
+/**
+ * Represents a game level, holding all its enemies, clouds,
+ * background layers, bottles, and coins.
+ */
 class Level {
 
     clouds;
@@ -7,6 +11,14 @@ class Level {
     bottles;
     coins;
 
+    /**
+     * Creates a new level with the given content.
+     * @param {MovableObject[]} enemies - Enemies in the level (e.g. Chicken, Endboss).
+     * @param {Cloud[]} clouds - Background clouds.
+     * @param {BackgroundObject[]} backgroundObjects - Background layer objects.
+     * @param {Bottle[]} bottles - Collectible bottles.
+     * @param {Coin[]} coins - Collectible coins.
+     */
     constructor(enemies, clouds, backgroundObjects, bottles, coins) {
         this.clouds = clouds;
         this.enemies = enemies;
