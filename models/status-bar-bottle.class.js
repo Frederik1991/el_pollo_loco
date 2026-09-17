3bb79728505f2aa8e4f877ed2e8186a0e1a4d561
Frederik Stoltenhoff <frederik.stoltenhoff@outlook.de>
2026-09-17 19:44:47 +0200

    style: add documentation comments for StatusBarBottle class and its methods

--- models/status-bar-bottle.class.js
+++ models/status-bar-bottle.class.js
@@ -1,3 +1,6 @@
+/**
+ * Status bar showing how many bottles the character has collected.
+ */
 class StatusBarBottle extends DrawableObject {
     IMAGES = [
         'img/7_statusbars/1_statusbar/3_statusbar_bottle/blue/0.png',
@@ -10,36 +13,49 @@ class StatusBarBottle extends DrawableObject {
 
     world;
 
+    /**
+     * Creates the bottle status bar, loads its images,
+     * and initializes it at 0%.
+     * @param {World} world - Reference to the game world.
+     */
     constructor(world) {
         super();
-         this.loadImages(this.IMAGES);
-         this.x = 20;
-         this.y = 55;
-         this.width = 220;
-         this.height = 60;
-         this.world = world;
-         this.setPercentage(0);
+        this.loadImages(this.IMAGES);
+        this.x = 20;
+        this.y = 55;
+        this.width = 220;
+        this.height = 60;
+        this.world = world;
+        this.setPercentage(0);
     }
 
+    /**
+     * Sets the current percentage and updates the displayed image.
+     * @param {number} percentage - Percentage of collected bottles (0-100).
+     */
     setPercentage(percentage) {
         this.percentage = percentage;
         let path = this.IMAGES[this.resolveImageIndex()];
         this.img = this.imageCache[path];
     }
 
-    resolveImageIndex () {
-    if(this.percentage == 100) {
-        return 5;
-    } else if(this.percentage >= 80) {
-        return 4;
-    } else if(this.percentage >= 60) {
-        return 3;
-    } else if(this.percentage >= 40) {
-        return 2;
-    } else if(this.percentage >= 20) {
-        return 1;
-    } else {
-        return 0;
+    /**
+     * Determines which image index matches the current percentage.
+     * @returns {number} Index into the IMAGES array.
+     */
+    resolveImageIndex() {
+        if (this.percentage == 100) {
+            return 5;
+        } else if (this.percentage >= 80) {
+            return 4;
+        } else if (this.percentage >= 60) {
+            return 3;
+        } else if (this.percentage >= 40) {
+            return 2;
+        } else if (this.percentage >= 20) {
+            return 1;
+        } else {
+            return 0;
+        }
     }
-}
 }
