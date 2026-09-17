aa5aac3c8faaf6f1ec7ca575a93da71327237700
Frederik Stoltenhoff <frederik.stoltenhoff@outlook.de>
2026-09-17 19:38:58 +0200

    style: format code indentation in Keyboard class

--- models/keyboard.class.js
+++ models/keyboard.class.js
@@ -1,9 +1,12 @@
+/**
+ * Stores the current pressed/released state of the keys used in the game.
+ */
 class Keyboard {
 
-left = false;
-right = false;
-up = false;
-down = false;
-space = false;
-d = false;
+    left = false;
+    right = false;
+    up = false;
+    down = false;
+    space = false;
+    d = false;
 }
